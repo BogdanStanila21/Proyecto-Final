451f469c00df5bc21701c1cdd8780b9769d3a13a
JoseRubioC <rubio.camino.jose@gmail.com>
2020-03-29 19:52:05 +0200

    error en el jquery

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { jQuery } from 'jquery';
+import * as $ from 'jquery';
+// import { jQuery } from 'jquery'
 
 @Component({
   selector: 'app-root',
