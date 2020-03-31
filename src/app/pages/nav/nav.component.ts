e3740f5888c56028eb902c05cbfffafb7899fd4d
JoseRubioC <rubio.camino.jose@gmail.com>
2020-03-31 23:21:05 +0200

    modificación del navbar

--- src/app/pages/nav/nav.component.ts
+++ src/app/pages/nav/nav.component.ts
@@ -12,33 +12,33 @@ export class NavComponent implements OnInit {
 
   ngOnInit(): void {}
 }
-/* jQuery para la barra de navegación */
-(function($) {
-  // Begin jQuery
-  $(function() {
-    // DOM ready
-    // If a link has a dropdown, add sub menu toggle.
-    $("nav ul li a:not(:only-child)").click(function(e) {
-      $(this)
-        .siblings(".nav-dropdown")
-        .toggle();
-      // Close one dropdown when selecting another
-      $(".nav-dropdown")
-        .not($(this).siblings())
-        .hide();
-      e.stopPropagation();
-    });
-    // Clicking away from dropdown will remove the dropdown class
-    $("html").click(function() {
-      $(".nav-dropdown").hide();
-    });
-    // Toggle open and close nav styles on click
-    $("#nav-toggle").click(function() {
-      $("nav ul").slideToggle();
-    });
-    // Hamburger to X toggle
-    $("#nav-toggle").on("click", function() {
-      this.classList.toggle("active");
-    });
-  }); // end DOM ready
-})(jQuery); // end jQuery
+// /* jQuery para la barra de navegación */
+// (function($) {
+//   // Begin jQuery
+//   $(function() {
+//     // DOM ready
+//     // If a link has a dropdown, add sub menu toggle.
+//     $("nav ul li button:not(:only-child)").click(function(e) {
+//       $(this)
+//         .siblings(".nav-dropdown")
+//         .toggle();
+//       // Close one dropdown when selecting another
+//       $(".nav-dropdown")
+//         .not($(this).siblings())
+//         .hide();
+//       e.stopPropagation();
+//     });
+//     // Clicking away from dropdown will remove the dropdown class
+//     $("html").click(function() {
+//       $(".nav-dropdown").hide();
+//     });
+//     // Toggle open and close nav styles on click
+//     $("#nav-toggle").click(function() {
+//       $("nav ul").slideToggle();
+//     });
+//     // Hamburger to X toggle
+//     $("#nav-toggle").on("click", function() {
+//       this.classList.toggle("active");
+//     });
+//   }); // end DOM ready
+// })($); // end jQuery
