1dfa353be18d9031bd7cf097b14f147ee68a9f9e
Jose Antonio Gea Falcon <geafalconjoseantonio@gmail.com>
2020-04-13 17:10:37 +0200

    cambios proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,10 @@ import { PerfilComponent } from "./pages/perfil/perfil.component";
 import { FavoritosComponent } from "./pages/favoritos/favoritos.component";
 import { MisLibrosComponent } from "./pages/mis-libros/mis-libros.component";
 import { SolicitudesComponent } from "./pages/solicitudes/solicitudes.component";
-import { ModalIntercambioComponent } from "./pages/modal-intercambio/modal-intercambio.component";
 import { LoginComponent } from "./pages/login/login.component";
 import { RegistroComponent } from "./pages/registro/registro.component";
-import { HomeComponent } from "./pages/home/home.component";
 import { NavComponent } from "./pages/nav/nav.component";
 import { PeticionesComponent } from "./pages/peticiones/peticiones.component";
-import { MasInformacionComponent } from "./pages/mas-informacion/mas-informacion.component";
 
 const routes: Routes = [
   { path: "", redirectTo: "login", pathMatch: "full" },
@@ -20,13 +17,9 @@ const routes: Routes = [
   { path: "favoritos", component: FavoritosComponent },
   { path: "misLibros", component: MisLibrosComponent },
   { path: "solicitudes", component: SolicitudesComponent },
-  { path: "intercambio", component: ModalIntercambioComponent },
   { path: "login", component: LoginComponent },
   { path: "registro", component: RegistroComponent },
-  { path: "home", component: HomeComponent },
-  // { path: "nav", component: NavComponent },
   { path: "peticiones", component: PeticionesComponent },
-  { path: "mas", component: MasInformacionComponent },
 ];
 
 @NgModule({
