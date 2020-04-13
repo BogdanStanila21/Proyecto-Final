a2732ca8adf59a66db17680d71a1458b85c2ddf0
Bogdan Stanila <bogdanstanila2190@gmail.com>
2020-04-13 21:27:46 +0200

    control de errores registro,login

--- src/app/pages/registro/registro.component.ts
+++ src/app/pages/registro/registro.component.ts
@@ -3,6 +3,7 @@ import { FormGroup, FormControl, Validators, FormBuilder } from "@angular/forms"
 import { passValidation } from "./custom-validator";
 import { Registro } from './../../models/registro';
 import { ApisService } from './../../service/apis.service';
+import {  Router } from '@angular/router';
 
 @Component({
   selector: "app-registro",
@@ -13,8 +14,10 @@ import { ApisService } from './../../service/apis.service';
 export class RegistroComponent implements OnInit {
   registroForm: FormGroup;
   public miRegistro: Registro;
-  constructor(private fb: FormBuilder, private ApiUser: ApisService) {
+  public users:Registro[];
+  constructor(private fb: FormBuilder, private ApiUser: ApisService, private api:ApisService, private router:Router) {
     this.buildForm();
+    this.getUsers();
   }
   // Validadores front
   buildForm() {
@@ -45,6 +48,14 @@ export class RegistroComponent implements OnInit {
       { validators: passValidation.matchPass }
     );
   }
+
+  getUsers(){
+    return this.api.getUsers().subscribe((data:Registro[])=>{
+      this.users=data;
+      console.log(data)
+    })
+  }
+
 // Añadir usuario
   addUser(nick:string,name:string,sexo:string,email:string,place:string,pass:string){
     let user=new Registro;
@@ -54,9 +65,37 @@ export class RegistroComponent implements OnInit {
     user.email=email;
     user.place=place;
     user.password=pass;
-    return this.ApiUser.postUser(user).subscribe((data)=>{
-      console.log(data);
-    })
+    console.log(user)
+    let check=false
+    let check2=false
+    let check3=false
+    for(let i=0;i<this.users.length;i++){
+      if(this.users[i].nickname==nick){
+        check=true
+      }if(this.users[i].name==name){
+        check2=true
+      }if(this.users[i].email==email){
+        check3=true
+      }
+    }
+    if (check===true){
+      alert("Nickname no valido")
+    }
+    if (check2===true){
+      alert("Name no valido")
+    }
+    if (check3===true){
+      alert("Email no valido")
+    }
+    if((check==false)&&(check2==false)&&(check3==false)){
+      return this.ApiUser.postUser(user).subscribe((data)=>{
+        console.log(data);
+        this.router.navigate(['/login'])
+        alert("Usuario registrado satisfactoriamente")
+      })
+    }else{
+      console.log("Error desconocido")
+    }    
   }
 // Cojer los datos de registro y ver por consola
   onSubmit() {
